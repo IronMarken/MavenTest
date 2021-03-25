4bf97d0b41ebad61d2680bc86f7fb79e5d6bdbaf
IronMarken <56649439+IronMarken@users.noreply.github.com>
2021-03-25 22:15:55 +0100

    first commit

---
+++ src/main/java/it/marco/App.java
@@ -0,0 +1,11 @@
+package it.marco;
+
+import org.json.JSONArray;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
